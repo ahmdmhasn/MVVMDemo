fd15b8099bf58c2181d0f6bf67ff51d5ed6eef8c
Ahmed M. Hassan <ahmdmhasn@gmail.com>
2020-07-22 22:06:44 +0200

    Initial Commit

---
+++ MVVMDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MVVMDemo
+//
+//  Created by Ahmed M. Hassan on 7/22/20.
+//  Copyright © 2020 Unicom. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
